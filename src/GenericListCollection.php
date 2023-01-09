e357052f6d9bae1ad06c1dee8506eb645d87f3ed
Andreas Sundqvist <andreas@stefna.is>
2023-01-09 12:43:07 +0100

    [collection] Add generic list
    This is so that we don't have to extend the abstract implementation
    for every type we want

---
+++ src/GenericListCollection.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Stefna\Collection;
+
+/**
+ * @template T of object
+ * @extends AbstractListCollection<T>
+ */
+final class GenericListCollection extends AbstractListCollection
+{
+}
